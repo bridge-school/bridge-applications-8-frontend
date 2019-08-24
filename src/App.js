0300e7deb076e1059a3a7a642c6b2616ea1dff26
Ann Marie Doyle <am.doyle@yahoo.ca>
2019-08-24 19:51:29 -0400

    TECH-0: Add React Router DOM to poject and refctor App to use new package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { request } from "./backend-request";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   useEffect(() => {
@@ -10,23 +10,11 @@ function App() {
     };
     fetchData();
   });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+     <Route path="/" />
+   </Router>
   );
 }
 
