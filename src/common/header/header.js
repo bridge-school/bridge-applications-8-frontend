0066f10ca3e9c2c062187bcd9d244a5a076ffe1d
Ann Marie Doyle <am.doyle@yahoo.ca>
2019-08-27 20:28:58 -0400

    TECH-4A: Update folder structure - Add common folder for the common elements - Add admin folder for the admin specific elements - Add student forlder for student specific elements

--- src/common/header/header.js
+++ src/common/header/header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./header.css";
-import logo from "../images/bridge-logo.svg";
+import logo from "../../images/bridge-logo.svg";
 
 const Header = ({title}) => {
   return(
