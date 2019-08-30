4ecf9bd73a17eee7de22a842ebb5315aa3641006
Ann Marie Doyle <am.doyle@yahoo.ca>
2019-08-29 20:54:53 -0400

    TASK-1 Add create cohort route and starting component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useEffect } from "react";
 import "./App.css";
 import { request } from "./backend-request";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import LandingPage from './common/landing-page/landing-page';
 import AdminDashboard from './admin/admin-dashboard/admin-dashboard';
+import CreateCohort from './admin/create-cohort/create-cohort';
+import LandingPage from './common/landing-page/landing-page';
 import NoMatches from './common/no-matches/no-matches';
-import PageLayout from './common/page-layout/page-layout'
+import PageLayout from './common/page-layout/page-layout';
 
 function App() {
   useEffect(() => {
@@ -21,6 +22,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route path="/dashboard" component={AdminDashboard} />
+          <Route path="/create-cohort" component={CreateCohort} />
           <Route component={NoMatches} />
         </Switch>
       </PageLayout>
---
+++ src/admin/create-cohort/create-cohort.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CreateCohort = () => {
+  return (
+    <>
+      <h1>Create New Cohort Application Form</h1>
+      <p>Place holder for form</p>
+    </>
+  );
+}
+
+export default CreateCohort;
