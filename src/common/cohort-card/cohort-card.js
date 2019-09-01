b0a301520ce1fa761f1567b1c032a7bf59cbb8b2
Shangni <shangnihu@gmail.com>
2019-09-01 19:56:28 -0400

    Working on cohort cards

--- src/common/cohort-card/cohort-card.js
+++ src/common/cohort-card/cohort-card.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import tachyons from "styled-components-tachyons";
 
 const CohortCard = () => {
-    return (
-        <h2>Cohort Card Placeholder</h2>
-    );
+  const CohortCard = styled.div`
+    padding: 1.25em 0;
+  `;
+
+
+  return (
+    <CohortCard>
+      <h2>Cohort Name</h2>
+
+      <button>Placeholder Button Component</button>
+    </CohortCard>
+  );
 };
 
 export default CohortCard;
