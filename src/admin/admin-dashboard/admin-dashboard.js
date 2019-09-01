33c70791c15e008bbfc7c45d95da5349159987cb
Hyeeun Mok <41407354+HyeeunMok@users.noreply.github.com>
2019-09-01 19:58:11 -0400

    Ticket0 a create button (#8)
    * package-lock
    
    * add button component
    
    * add create button

--- src/admin/admin-dashboard/admin-dashboard.js
+++ src/admin/admin-dashboard/admin-dashboard.js
@@ -1,8 +1,26 @@
 import React from "react";
+import styled from 'styled-components';
+import tachyons from 'styled-components-tachyons';
+import Button from '../../common/button/button';
 
 const AdminDashboard = () => {
+  const H1 = styled.h1`
+    padding: 0;
+    margin: 0;
+    font-size: 2.2rem;
+    font-weight: bold;
+    ${tachyons}
+  `;
+
+  const Wrapper = styled.div`
+    ${tachyons}
+  `;
+
   return(
-    <h1>Dashboard Placeholder</h1>
+    <Wrapper flex justify_between>
+      <H1 di>Cohort Application Forms</H1>
+      <Button color="green" size="large">CREATE APPLICATION GROUP</Button>
+    </Wrapper>
   )
 }
 
