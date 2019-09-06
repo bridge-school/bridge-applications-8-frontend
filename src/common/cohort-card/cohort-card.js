0d3ff1f556622c38f2dfcc0164e43ef0949f71ef
Shangni Hu <shangnihu@gmail.com>
2019-09-05 20:16:42 -0400

    Story 0 b cohort cards (#10)
    * Created cohort card folder and base files.
    
    * Working on cohort cards
    
    * Added component for Cohort Card, added dummy data in to Admin Dashboard.

---
+++ src/common/cohort-card/cohort-card.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import tachyons from "styled-components-tachyons";
+import Button from "../../common/button/button";
+
+const CohortCard = ({name, type}) => {
+  const CohortCard = styled.div`
+    padding: 1.05rem 1.60rem;
+    display: block;
+    background: #F0F0F0;
+    margin: 0.8rem 0;
+    border-radius: 0.5rem;
+    ${tachyons}
+
+  `;
+
+  const H2 = styled.h2`
+    font-size: 1.3rem;
+    margin: 0;
+  `;
+
+  let buttonColor;
+
+  if (type==="frontend development") {
+    buttonColor = "pink";
+  } else if (type==="backend development") {
+    buttonColor = "blue";
+  } else {
+    buttonColor = "green";
+  }
+
+
+  return (
+    <CohortCard flex justify_between items_center>
+      <H2>{name}</H2>
+
+      <Button color={buttonColor} size="small">{type}</Button>
+    </CohortCard>
+  );
+};
+
+export default CohortCard;
