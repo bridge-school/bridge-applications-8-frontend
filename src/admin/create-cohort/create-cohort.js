c5d0f82097a5cbc4e015c68bf27b0e4fa73731ab
nlconway <natalielconway@gmail.com>
2019-09-08 10:40:20 -0400

    Task-1B: created create-form and select (#11)
    * Task-1B: created createform added to create-cohort
    
    * Removed unused import
    
    * removed p tag
    
    * Story 0 b cohort cards (#10)
    
    * Created cohort card folder and base files.
    
    * Working on cohort cards
    
    * Added component for Cohort Card, added dummy data in to Admin Dashboard.

--- src/admin/create-cohort/create-cohort.js
+++ src/admin/create-cohort/create-cohort.js
@@ -1,10 +1,11 @@
 import React from "react";
+import CreateForm from "./create-form"
 
 const CreateCohort = () => {
   return (
     <>
       <h1>Create New Cohort Application Form</h1>
-      <p>Place holder for form</p>
+      <CreateForm />
     </>
   );
 }
