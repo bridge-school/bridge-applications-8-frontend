ebd532d7bfba50ecb14c8dc93ae7f45b873cfe90
Ann Marie Doyle <am.doyle@yahoo.ca>
2019-09-10 19:33:13 -0400

    STORY-3: Pass Field data to custom select commponent

--- src/common/forms/custom-select.js
+++ src/common/forms/custom-select.js
@@ -15,7 +15,15 @@ const CustomSelect = ({
       <Error>
         <ErrorMessage name={field.name} />
       </Error>
-      <SelectField br2 ba bw1 w_100 name="cohorttype" component="select">
+      <SelectField
+        br2
+        ba
+        bw1
+        w_100
+        name="cohorttype"
+        component="select"
+        {...field}
+      >
         <option value="">{props.optionsLabel}</option>
         {props.options.map(option => (
           <option key={removeSpaces(option)} value={removeSpaces(option)}>
