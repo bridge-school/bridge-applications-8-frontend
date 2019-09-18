57e2b111c116df9b32846819bfe0b19dbf49f4a1
Hyeeun <hyeeuni.mok@gmail.com>
2019-09-17 20:10:10 -0400

    deleted LinkButton

--- src/applicant/applicant-dashboard/applicant-dashboard.js
+++ src/applicant/applicant-dashboard/applicant-dashboard.js
@@ -5,7 +5,6 @@ import getCohorts from "../../store/actions/getCohorts";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import tachyons from "styled-components-tachyons";
-import LinkButton from "../../common/button/link";
 import CohortCard from "../../common/cohort-card/cohort-card";
 import ApplicantForm from "../applicant-form/applicant-form";
 
